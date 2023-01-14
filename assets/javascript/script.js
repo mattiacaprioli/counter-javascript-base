d7becccd90706f567529ea97f9c02b5dec1eddaa
Mattia Caprioli <capriolimattia1994@gmail.com>
2023-01-14 17:18:56 +0800

    cambio colore p

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,15 +3,15 @@ function decrement() {
     var currentCount = parseInt(count.innerHTML);
     count.innerHTML = currentCount - 1;
     if(currentCount - 1 < 0){
-      count.classList.remove("text-indigo-500");
+      count.classList.remove("text-neutral-700");
       count.classList.add("text-red-500");
     }else if(currentCount - 1 > 0){
-      count.classList.remove("text-indigo-500");
+      count.classList.remove("text-neutral-700");
       count.classList.add("text-green-500");
     }else{
       count.classList.remove("text-red-500");
       count.classList.remove("text-green-500");
-      count.classList.add("text-indigo-500");
+      count.classList.add("text-neutral-700");
     }
   }
   function increment() {
@@ -19,15 +19,15 @@ function decrement() {
     var currentCount = parseInt(count.innerHTML);
     count.innerHTML = currentCount + 1;
     if(currentCount + 1 < 0){
-      count.classList.remove("text-indigo-500");
+      count.classList.remove("text-neutral-700");
       count.classList.add("text-red-500");
     }else if(currentCount + 1 > 0){
-      count.classList.remove("text-indigo-500");
+      count.classList.remove("text-neutral-700");
       count.classList.add("text-green-500");
     }else{
       count.classList.remove("text-red-500");
       count.classList.remove("text-green-500");
-      count.classList.add("text-indigo-500");
+      count.classList.add("text-neutral-700");
     }
   }
   function reset() {
@@ -35,5 +35,5 @@ function decrement() {
     count.innerHTML = 0;
     count.classList.remove("text-red-500");
     count.classList.remove("text-green-500");
-    count.classList.add("text-indigo-500");
+    count.classList.add("text-neutral-700");
   }
